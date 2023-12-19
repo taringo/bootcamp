83cb70035d8c5e9b2fdcf66cee204bed428245b2
taringo ayeli <taringo4@gmail.com>
2023-12-19 21:10:11 +0200

    week3 day1

---
+++ week3/day2/script.js
@@ -0,0 +1,12 @@
+let food= 'meet'
+
+let meal= 'lunch'
+
+console.log ("i eat " + food + " at every " + meal)
+
+let c = 30
+
+let f = c/5*9+32 
+
+console.log(`${c}°C is ${f}°F`);
+
---
+++ week3/day3/script.js
@@ -0,0 +1,28 @@
+let name = 'value'
+let str = 'text'
+let num = 6
+let bool = true
+let un;
+let v = null  
+let array = [12, 15, 17, 'num']
+let object = {
+    key: 'value',
+    name: 'Nadav'
+}
+let x = str.indexOf('x')
+// console.log(x); 
+
+// ---------------------------
+
+let str2 = 'hello, its me '+ str + num + bool
+let str3 = "hello it's me" 
+let str4 = `${str} text ${num}`
+
+// console.log(str4);
+
+
+let sentence = 'Hello Sarah, HOW ARE YOU?'
+// console.log(sentence.toLowerCase());
+// console.log('hello' * 5);
+
+console.log(10.6569842486.toFixed(0));
