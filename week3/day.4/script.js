801a6bb769dfb61e540a5807d195d64fbb556fe8
taringo ayeli <taringo4@gmail.com>
2023-12-21 21:14:04 +0200

    week3 day5

---
+++ week3/day.4/script.js
@@ -0,0 +1,82 @@
+
+
+let mydata ={
+
+    name: 'taringo', 
+
+    lastname: 'ayeli',
+
+    age:'36',
+
+}
+
+console.log(mydata.name)
+
+
+
+let obj ={
+    username: 'taringo4',
+   
+    password: '0509777424',
+
+    name: 'yaffa',
+
+    age: '36',
+}
+
+obj.age = '37'
+
+obj.eyecolore = 'black'
+
+delete obj.name
+
+console.log(obj);
+
+let userdata ={
+
+username: 'ayelitaringo',
+
+password: '0509777424',
+
+}
+
+console.log(userdata);
+
+
+let myAge =2
+
+if (myAge > 21)
+
+console.log('nadav can drink');
+
+else if (myAge > 18 && myAge < 21) {
+
+    console.log("nadav can drink, not in the us");
+    
+}
+
+else {
+    console.log("nadav can't drink");
+}
+
+if (25 !== '25') {
+    console.log('maybe?');
+}
+
+let password = '68769';
+
+if (password.length > 8) {
+    console.log('password is 0k');
+} 
+else if (password > 18 && password <8 ) {
+
+    console.log('password is ok');
+
+}
+
+else {
+    console.log('password is too short');
+}
+
+
+
--- week3/day2/script.js
+++ week3/day2/script.js
@@ -4,9 +4,44 @@ let meal= 'lunch'
 
 console.log ("i eat " + food + " at every " + meal)
 
+
+
 let c = 30
 
 let f = c/5*9+32 
 
 console.log(`${c}°C is ${f}°F`);
+
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory" ,"friend"];
+
+let myWatchedSeriesLength = myWatchedSeries.length
+
+console.log(myWatchedSeries); 
+
+let myWatchedSeriesSentence = myWatchedSeries.join()
+
+console.log(myWatchedSeriesSentence);
+
+
+
+
+let d;
+let a = 34;
+let b = 21;
+
+console.log(34+21) //first expression
+// Prediction:
+// Actual:
+
+a = 2;
+
+console.log(2+21) //second expression
+// Prediction:
+// Actual:
+
+console.log(d);
+
+console.log(3 + 4 + '5');
+
+
 
